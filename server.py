53499e3a18cdf13efbbf587b9a528fc68f0d88de
Yorik <yorikkshotter@gmail.com>
2025-09-16 21:58:53 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 # create the app
 app = Flask(__name__)
@@ -27,4 +28,6 @@ def greet(name):
 
 # run the server
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
